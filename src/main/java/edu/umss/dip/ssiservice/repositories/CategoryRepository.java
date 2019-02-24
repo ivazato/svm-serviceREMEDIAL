1d80ce85a21b2c55ca09c5b0f95aa8ed66fcb680
Edson Terceros <edson.terceros@avantica.net>
2019-02-24 18:25:02 -0400

    query personalizado findByCode usando JPA

--- src/main/java/edu/umss/dip/ssiservice/repositories/CategoryRepository.java
+++ src/main/java/edu/umss/dip/ssiservice/repositories/CategoryRepository.java
@@ -6,6 +6,10 @@ package edu.umss.dip.ssiservice.repositories;
 
 import edu.umss.dip.ssiservice.model.Category;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface CategoryRepository extends GenericRepository<Category> {
+    Optional<List<Category>> findByCode(String code);
 }
   
--- src/main/java/edu/umss/dip/ssiservice/repositories/SubCategoryRepository.java
+++ src/main/java/edu/umss/dip/ssiservice/repositories/SubCategoryRepository.java
@@ -4,8 +4,13 @@
 
 package edu.umss.dip.ssiservice.repositories;
 
+import edu.umss.dip.ssiservice.model.Category;
 import edu.umss.dip.ssiservice.model.SubCategory;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface SubCategoryRepository extends GenericRepository<SubCategory> {
+    Optional<List<Category>> findByCode(String code);
 }
   
