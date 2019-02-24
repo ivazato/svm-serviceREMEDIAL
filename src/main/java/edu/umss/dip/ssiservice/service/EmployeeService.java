6c3cb2e3de6b5f08e5d3f6ea0922de13fef01489
Edson Terceros <edson.terceros@avantica.net>
2019-02-24 16:52:46 -0400

    servicio para empleado usando template para servicios

---
+++ src/main/java/edu/umss/dip/ssiservice/service/EmployeeService.java
@@ -0,0 +1,12 @@
+/**
+ * @author: Edson A. Terceros T.
+ */
+
+package edu.umss.dip.ssiservice.service;
+
+import edu.umss.dip.ssiservice.model.Employee;
+
+public interface EmployeeService extends GenericService<Employee> {
+}
+
+    
---
+++ src/main/java/edu/umss/dip/ssiservice/service/EmployeeServiceImpl.java
@@ -0,0 +1,24 @@
+/**
+ * @author: Edson A. Terceros T.
+ */
+
+package edu.umss.dip.ssiservice.service;
+
+import edu.umss.dip.ssiservice.model.Employee;
+import edu.umss.dip.ssiservice.repositories.EmployeeRepository;
+import edu.umss.dip.ssiservice.repositories.GenericRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeServiceImpl extends GenericServiceImpl<Employee> {
+    private final EmployeeRepository repository;
+
+    public EmployeeServiceImpl(EmployeeRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    protected GenericRepository<Employee> getRepository() {
+        return repository;
+    }
+}
