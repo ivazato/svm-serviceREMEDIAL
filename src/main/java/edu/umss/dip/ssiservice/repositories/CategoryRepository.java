c62bd15f88d5b44d926e19490b5c8330e3a50fa9
Edson Terceros <edson.terceros@avantica.net>
2019-02-24 19:25:32 -0400

    make use list instead optional

--- src/main/java/edu/umss/dip/ssiservice/repositories/CategoryRepository.java
+++ src/main/java/edu/umss/dip/ssiservice/repositories/CategoryRepository.java
@@ -7,9 +7,8 @@ package edu.umss.dip.ssiservice.repositories;
 import edu.umss.dip.ssiservice.model.Category;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface CategoryRepository extends GenericRepository<Category> {
-    Optional<List<Category>> findByCode(String code);
+    List<Category> findByCode(String code);
 }
   
