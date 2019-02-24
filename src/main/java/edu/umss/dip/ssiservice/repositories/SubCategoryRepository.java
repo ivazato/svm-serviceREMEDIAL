52be12f4053c95ac72bf2f4c6f22c4e882fd10d8
Edson Terceros <edson.terceros@avantica.net>
2019-02-24 16:07:42 -0400

    completar repositorios faltaltes

---
+++ src/main/java/edu/umss/dip/ssiservice/repositories/SubCategoryRepository.java
@@ -0,0 +1,12 @@
+/**
+ * @author: Edson A. Terceros T.
+ */
+
+package edu.umss.dip.ssiservice.repositories;
+
+import edu.umss.dip.ssiservice.model.SubCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubCategoryRepository extends JpaRepository<SubCategory, Long> {
+}
+  
