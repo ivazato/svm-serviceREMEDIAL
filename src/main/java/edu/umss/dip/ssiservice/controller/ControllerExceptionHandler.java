e916cae0ef34513a48c0b54b69f9a4ed3af4a57f
Edson Terceros <edson.terceros@avantica.net>
2019-02-27 02:13:57 -0400

    application error handling

---
+++ src/main/java/edu/umss/dip/ssiservice/controller/ControllerExceptionHandler.java
@@ -0,0 +1,24 @@
+/**
+ * @author: Edson A. Terceros T.
+ */
+
+package edu.umss.dip.ssiservice.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(NumberFormatException.class)
+    public ModelAndView handleNumberFormat(Exception e) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("applicationerror");
+        modelAndView.addObject("exception", e);
+        return modelAndView;
+    }
+}
